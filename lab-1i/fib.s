70eed89b8b2423322611950773c1f4c512666289
zeroplusone <a711186@gmail.com>
2015-09-30 01:58:18 +0800

    modify lab-1i/fib.s to implement iterative version
    Use r3 to count, r4 to record fib(n-1), r5 to record fib(n-3), r0 to record answer.
    The formula in each loop is r0=r4+r5, and loop for r3 times.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,30 +13,33 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	@ R3 = R0 (update flags, used to count)
+	mov r3, r0
+	@ R0 = 1 (initialization, equals to fib(1))
+	mov r0, #1
+	@ R4 = 1 (initialization, used to record fin(n-1))
+	mov r4, #1
+	@ R5 = 0 (initialization, used to record fib(n-2))a
+	mov r5, #0
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+.loop:
+	@ R3 = R3 - 1 (update counts)
+	sub r3, r3, #1
+	@ if(R3 == 0) goto .over
+	cmp r3, #0
+	beq .over
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	@ R0 = R4 + R5
+	add r0, r4, r5
+	@ R5 = R4 (update)
+	mov r5, r4
+	@ R4 = R0 (update)
+	mov r4, r0
+	@ goto .loop (continue to calculate)
+	bl .loop
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
+.over:
 	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
